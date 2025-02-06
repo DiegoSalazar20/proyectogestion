1f58afc41343ae0c9a69c8298d711c871fb66d31
RitchelS <sojoritchel@gmail.com>
2025-02-05 18:30:11 -0600

    Página principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'principal', pathMatch: 'full' }, 
+    { path: 'principal', component: PrincipalComponent }
+  
+    
+  ];
