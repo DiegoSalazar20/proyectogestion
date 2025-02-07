bcb3a2039b9093406ff35537d987da7e90965718
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-02-06 22:45:50 -0600

    Ver publicaciones y ver respuestas

--- src/app/publicaciones/publicaciones.component.ts
+++ src/app/publicaciones/publicaciones.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../menu/menu.component';
+
 @Component({
   selector: 'app-publicaciones',
-  imports: [MenuComponent],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MenuComponent, HttpClientModule],
   templateUrl: './publicaciones.component.html',
   styleUrl: './publicaciones.component.scss'
 })
-export class PublicacionesComponent {
+export class PublicacionesComponent implements OnInit {
+  publicaciones: any[] = [];
+  nuevaPublicacion = { texto: '', imagen: '' };
 
+  modalVisible = false; // Controla la visibilidad del modal
+  respuestasActuales: any[] = []; // Almacena las respuestas para el modal
+  publicacionSeleccionada: any; // Almacena la publicación seleccionada
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.obtenerPublicaciones();
+  }
+
+  obtenerPublicaciones() {
+    this.http.get<any[]>('https://localhost:7004/api/Publicacion/todos').subscribe({
+      next: (data) => {
+        this.publicaciones = data.filter(p => p.activo);
+      },
+      error: (err) => {
+        console.error('Error obteniendo publicaciones:', err);
+      }
+    });
+  }
+
+  publicar() {
+    if (this.nuevaPublicacion.texto.trim() !== '') {
+      const nueva = {
+        idPublicacion: this.publicaciones.length + 1,
+        idUsuario: 1,
+        texto: this.nuevaPublicacion.texto,
+        imagen: this.nuevaPublicacion.imagen || 'https://via.placeholder.com/150',
+        fecha: new Date().toISOString(),
+        activo: true
+      };
+      this.publicaciones.unshift(nueva);
+      this.nuevaPublicacion = { texto: '', imagen: '' };
+    }
+  }
+
+
+  abrirModal(idPublicacion: number) {
+    this.publicacionSeleccionada = this.publicaciones.find(p => p.idPublicacion === idPublicacion);
+    if (this.publicacionSeleccionada) {
+      this.http.get<any[]>(`https://localhost:7004/api/Respuesta/todos?idRespuesta=${idPublicacion}`)
+        .subscribe(respuestas => {
+          console.log("Respuestas recibidas:", respuestas);
+          this.respuestasActuales = respuestas;
+          this.modalVisible = true; // Mostrar el modal
+        }, error => {
+          console.error("Error obteniendo respuestas:", error);
+        });
+    }
+  }
+
+  // Método para cerrar el modal
+  cerrarModal() {
+    this.modalVisible = false;
+    this.respuestasActuales = []; // Limpiar las respuestas
+  }
 }
